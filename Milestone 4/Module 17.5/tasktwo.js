c29281aa1c91c2bd284f66f36375657eb416e81c
omarfarukbadhon <omarfarukbadhon@gmail.com>
2024-01-29 16:14:27 +0600

    module 17.5 task two done

---
+++ Milestone 4/Module 17.5/tasktwo.js
@@ -0,0 +1,8 @@
+let booklet = ['Harry Porter', 'Mathrones', 'Chemistry', 'English', 'Bangla', 'Physics'];
+
+if (booklet.includes('javascript book')){
+    console.log("Present");
+}else{
+    console.log('Absent');
+}
+
