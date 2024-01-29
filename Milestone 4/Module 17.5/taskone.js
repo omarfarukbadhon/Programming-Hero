0316a1777718b3a84edffae2e99643d65c5a3363
omarfarukbadhon <omarfarukbadhon@gmail.com>
2024-01-29 16:05:48 +0600

    module 17.5 task one done

---
+++ Milestone 4/Module 17.5/taskone.js
@@ -0,0 +1,9 @@
+let fruits = ['Mango', 'Orange', 'Guava', 'watermelon', 'Jackfruit'];
+
+console.log(fruits[3]);
+
+let secondIndex = fruits.indexOf('Guava');
+
+fruits[secondIndex] = 'Jambura';
+
+console.log(fruits);
