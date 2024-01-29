e105d6a12decd89ba64a329707680551aeed37ca
omarfarukbadhon <omarfarukbadhon@gmail.com>
2024-01-29 16:25:19 +0600

    module 17.5 task three done

---
+++ Milestone 4/Module 17.5/taskthree.js
@@ -0,0 +1,10 @@
+let touristSpot = ['Sent Martin', 'Sajek', 'Sundarban', 'Cox\'s Bazar'];
+
+touristSpot.push('Darjiling');
+ 
+touristSpot.push('Kashmir', 'Bali');
+
+touristSpot.pop();
+
+console.log(touristSpot);
+
