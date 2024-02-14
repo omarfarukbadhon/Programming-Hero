ca7cec739f96473a383b12a377893333abd7d1a5
omarfarukbadhon <omarfarukbadhon@gmail.com>
2024-02-14 23:10:50 +0600

    module 22 added

--- Milestone 4/Module 18/skip-odd.js
+++ Milestone 4/Module 18/skip-odd.js
@@ -1,6 +1,12 @@
-for(let i = 50; i<=80; i++){
-    if(i % 2 !== 0){
-        continue;
-    }
-    console.log(i);
-}
+// for(let i = 50; i<=80; i++){
+//     if(i % 2 !== 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+const str = "Hello";
+str[0] = "h";
+console.log(str);
